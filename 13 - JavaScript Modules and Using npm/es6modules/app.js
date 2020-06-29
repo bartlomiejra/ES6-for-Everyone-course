c4f215024619138a043988eb5cc8a8be5141bea3
Bart <bartlomiej.ra@gmail.com>
2020-06-29 21:01:15 +0200

    update

--- 13 - JavaScript Modules and Using npm/es6modules/app.js
+++ 13 - JavaScript Modules and Using npm/es6modules/app.js
@@ -1,4 +1,9 @@
 import { uniq} from 'lodash';
 import insane from 'insane';
 import jsonp from 'jsonp';
- 
+ import apiKey from './src/config';
+ console.log(apiKey) 
+
+const ages = [1,1,4,52,12,4];
+
+console.log(uniq(ages));
